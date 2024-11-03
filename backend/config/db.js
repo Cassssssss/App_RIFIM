373da4b92afc29f2f97a230f85750eac9c4eb33f
Cass <cassiel-94@hotmail.fr>
2024-11-03 19:35:19 +0100

    Résolution des conflits

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('MongoDB connecté');
   } catch (error) {
     console.error('Erreur de connexion MongoDB:', error.message);
